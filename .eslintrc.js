7e69b1fcf83507af45281699851026c7b413be85
linhaishe <42030043+linhaishe@users.noreply.github.com>
2024-04-11 20:08:45 +0800

    feat: add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:import/typescript',
     'plugin:markdown/recommended',
+    'plugin:storybook/recommended',
   ],
   env: {
     browser: true,
